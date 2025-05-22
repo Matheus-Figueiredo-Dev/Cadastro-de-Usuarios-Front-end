6f9d7d670780d2d9f520b83ba4ca2a0b2d4d0458
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-05-22 10:02:05 -0300

    Buscando usuários na API

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import DefaultButton from '../../components/Button';
 import DefaultTopBackground from '../../components/TopBackground';
+import api from '../../services/api';
 
 export function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const { data } = await api.get('/users');
+      setUsers(data);
+    }
+    loadUsers();
+  }, []);
+
   return (
     <div>
       <DefaultTopBackground />
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.age}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
       <DefaultButton>Voltar</DefaultButton>
     </div>
   );
